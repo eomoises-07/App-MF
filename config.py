0d20c47ff515eed1141f33013f21c3444038d9d3
eomoises-07 <moisesrose2020@gmail.com>
2025-04-30 19:01:09 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Arquivo de Configuração para Forex Alpha Signals 2.0
+
+# -- Credenciais --
+# Substitua pelos seus valores reais
+SENHA_APP = "Deuséfiel"
+TELEGRAM_TOKEN = "7721305430:AAG1f_3Ne79H3vPkrgPIaJ6VtrM4o0z62ws"
+TELEGRAM_CHAT_ID = "5780415948"
+
+# -- Parâmetros (podem ser adicionados mais tarde) --
+# Exemplo: PARAMETRO_X = 123
+
